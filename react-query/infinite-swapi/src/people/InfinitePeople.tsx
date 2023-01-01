6d46a034fcc9a44a3a168949ee970ab5b2d9b12e
jong-k <kjhan710@naver.com>
2023-01-01 23:01:51 +0900

    feature: Infinite Scroll 셋업

--- react-query/infinite-swapi/src/people/InfinitePeople.tsx
+++ react-query/infinite-swapi/src/people/InfinitePeople.tsx
@@ -1,13 +1,17 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
-const fetchUrl = async (url) => {
+const fetchUrl = async (url: string) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery();
+
   return <InfiniteScroll />;
 }
