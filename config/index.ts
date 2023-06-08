c674c50a2c1f1abaebaccd431466f1340775dede
jong-k <kjhan710@naver.com>
2023-06-08 12:25:56 +0900

    feat: cloudinary 동영상 경로 추가

--- config/index.ts
+++ config/index.ts
@@ -2,3 +2,5 @@ export const SERVER_URL = process.env.NEXT_PUBLIC_API_URL;
 export const CLIENT_URL = process.env.NEXT_PUBLIC_CLIENT_URL;
 export const MAP_KEY = process.env.NEXT_PUBLIC_MAP_KEY;
 export const PER_PAGE = 5;
+export const VIDEO_URL =
+  "https://res.cloudinary.com/dcdc0dc7u/video/upload/v1682909862/samples/elephants.mp4";
