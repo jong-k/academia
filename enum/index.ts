9e2cead58b19654e0d6785918a37a69e6cf598a0
jong-k <kjhan710@naver.com>
2023-06-20 19:49:16 +0900

    chore: 캐러셀 디폴트 데이터 추가

--- enum/index.ts
+++ enum/index.ts
@@ -2,3 +2,27 @@ export const BANNER_TEXT = {
   TITLE: "아카데미아",
   SUB_TITLE: "스터디 메이트를 찾고 포럼에 참여해보세요!",
 };
+
+export const CAROUSEL_CONTENTS_LIST = [
+  {
+    ID: 0,
+    TITLE: "여기는 타이틀1 입니다",
+    SUB_TITLE: "그리고 여기는 서브타이틀1 입니다",
+    IMAGE: "./images/sample-slide1.svg",
+    BACKGROUND_COLOR: "tomato",
+  },
+  {
+    ID: 1,
+    TITLE: "여기는 타이틀2 입니다",
+    SUB_TITLE: "그리고 여기는 서브타이틀2 입니다",
+    IMAGE: "./images/sample-slide2.svg",
+    BACKGROUND_COLOR: "teal",
+  },
+  {
+    ID: 2,
+    TITLE: "여기는 타이틀3 입니다",
+    SUB_TITLE: "그리고 여기는 서브타이틀3 입니다",
+    IMAGE: "./images/sample-slide3.svg",
+    BACKGROUND_COLOR: "thistle",
+  },
+];
